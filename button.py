6fe050aa3bf43d7aa4f79f56436af4ebdb06fd34
Maahir-Paliwal <maahir.paliwal@mail.mcgill.ca>
2025-06-14 19:50:01 -0400

    button functionality

--- button.py
+++ button.py
@@ -3,12 +3,18 @@
 from gpiozero import Button
 from signal import pause
 
-button = Button(26, pull_up = True)
+button = Button(26, pull_up = True, hold_time=5.0)
 
 def hello():
     print("I am a button")
 
+
+def held():
+    print("I am being held")
+
+
 #give it the function id
 button.when_pressed = hello
+button.when_held = held
 
 pause()
